43e5e6e6929fd0bf9984cb76a6dfa2d10e0daed3
superBigHuang <841069102@qq.com>
2020-07-06 21:56:19 +0800

    配置第三方loader和webpack-dev-server即其常用命令

--- webpack/src/main.js
+++ webpack/src/main.js
@@ -4,8 +4,16 @@
 //由于ES6d的代码太高级了,浏览器解析不了，所以会报错
 import $ from 'jquery'  //相当于 const $ = require('jquery')
 
+// 使用import语法，写入css样式表
+import './css/index.css'
+import './css/index2.less'
+// 注意：webpack默认只能打包处理js文件
+// 如果要处理非JS文件，我们需要手动的安装一些第三方loader加载器
+// 如果需要打包处理css 需要安装 npm i style-loader css-loader -D
+// 打开webpack.config.js这个配置文件，在里面新增一个module节点
+
 $(function () {
-    $('li:odd').css('backgroundColor', 'red')
+    $('li:odd').css('backgroundColor', 'black')
     $('li:even').css('backgroundColor', function () {
             return 'yellow'
     })
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,6 +1,10 @@
 //webpack配置文件
 
 const path = require('path')
+const webpack = require('webpack')
+//导入在内存中生成HTML页面的插件
+//只要是插件都一定要放到plugins节点中去
+const htmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
     //手动指定入口和出口
@@ -8,6 +12,27 @@ module.exports = {
     output: { //出口
         path: path.join(__dirname, './dist'),
         filename: 'bundle.js' //指定输出的名称
+    },
+    devServer: { //这是配置dev-server命令参数的第二种形式，相对来说麻烦一点
+        open: true, // 自动打开浏览器
+        port: 3000, // 设置启动时候的项目端口
+        contentBase: 'src', //指定托管的根目录
+        hot: true // 启用热更新 的 第一步
+
+    },
+    // 启动热更新的第二步
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),  //new 一个热更新的模块对象
+        new htmlWebpackPlugin({
+            template: path.join(__dirname, './src/index.html'), //指定模板页面,将来会根据指定的页面路径，去生成内存中的页面
+            filename: 'index123.html', //指定页面的名称
+        })
+    ],
+    module: { //这个节点用于配置所有的第三方模块加载器
+        rules: [ // 所有第三方模块的匹配规则
+            {test: /\.css$/, use: ['style-loader', 'css-loader']},  // 配置处理css文件的第三方loader规则
+            {test: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader']} // 配置处理less文件的第三方loader规则
+        ]
     }
 
 }
